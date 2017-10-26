a05d4660c45249eaa05ebd55b43e27714c15cb98
panorius <bowox77127@hotmail.fr>
2017-10-26 16:10:09 +0200

    [HTML] Rename
    type of index.html to index.php
    head>title, header>h1
    nav>li and ul (name modified)

--- V1/index.php
+++ V1/index.php
@@ -6,7 +6,7 @@
 -->
 <html>
 	<head>
-		<title>TXT by HTML5 UP</title>
+		<title>VieTaGrimpe</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
@@ -20,8 +20,7 @@
 				<header id="header">
 					<div class="logo container">
 						<div>
-							<h1><a href="index.html" id="logo">TXT</a></h1>
-							<p>A responsive site template by HTML5 UP</p>
+							<h1><a href="index.php" id="logo">vie ta grimpe</a></h1>
 						</div>
 					</div>
 				</header>
@@ -29,27 +28,21 @@
 			<!-- Nav -->
 				<nav id="nav">
 					<ul>
-						<li class="current"><a href="index.html">Home</a></li>
+						<li class="current"><a href="#">Accueil</a></li>
 						<li>
-							<a href="#">Dropdown</a>
+							<a href="#">Competitions</a>
 							<ul>
-								<li><a href="#">Lorem ipsum dolor</a></li>
-								<li><a href="#">Magna phasellus</a></li>
-								<li>
-									<a href="#">Phasellus consequat</a>
-									<ul>
-										<li><a href="#">Lorem ipsum dolor</a></li>
-										<li><a href="#">Phasellus consequat</a></li>
-										<li><a href="#">Magna phasellus</a></li>
-										<li><a href="#">Etiam dolore nisl</a></li>
-									</ul>
-								</li>
-								<li><a href="#">Veroeros feugiat</a></li>
+								<li><a href="#">Calendrier</a></li>
 							</ul>
 						</li>
-						<li><a href="left-sidebar.html">Left Sidebar</a></li>
-						<li><a href="right-sidebar.html">Right Sidebar</a></li>
-						<li><a href="no-sidebar.html">No Sidebar</a></li>
+						<li><a href="#">Divers</a>
+                            <ul>
+                                <li><a href="#">Grimpeurs</a></li>
+                                <li><a href="#">Carte</a></li>
+                            </ul>
+                        </li>
+						<li><a href="#">Photos</a></li>
+						<li><a href="#">Contact</a></li>
 					</ul>
 				</nav>
 
